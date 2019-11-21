02ada0f6de92d9e36a996e35ea48c56564d9a4b6
sergey <abramizsaransk@gmail.com>
2019-11-21 20:20:08 +0300

    Good work version

--- index.js
+++ index.js
@@ -1,4 +1,89 @@
-function sum(a, b) {
-  return a + b;
+"use strict";
+var through = require('through2');
+var PluginError = require('plugin-error');
+
+//имя плагина
+const PLUGIN_NAME = 'gulp-delete-js-debug';
+
+function generate_reg_string(debugtype) {
+    var strmas=[];
+    if (debugtype.indexOf( 'debugger' ) !== -1) strmas.push("debugger;");
+    if ((debugtype.indexOf( 'console' ) !== -1)||(debugtype.indexOf( 'window.console' ) !== -1)) {
+        var str="("
+        if (debugtype.indexOf( 'console' ) !== -1){
+            str+="console";
+            if (debugtype.indexOf( 'window.console' ) !== -1) str+="|";
+        }
+        if (debugtype.indexOf( 'window.console' ) !== -1) str+="window.console";
+        str+=").";
+        str+="(?:log|info|warn|error|assert|count|clear|group|groupEnd|groupCollapsed|trace|debug|dir|dirxml|profile|profileEnd|time|timeEnd|timeStamp|table|exception)" +
+            "\\([^;]*\\);?";
+        strmas.push(str);
+    }
+    return strmas.join("|");
 }
-module.exports = sum;
+
+function obrabot(options) {
+    //поток, по которому будут проходить все файлы
+    var stream = through.obj(function(file, enc, cb) {
+        if (file.isBuffer()) {
+            options = options || {};
+            //если файл пуст
+            if (file.isNull()) {
+                //вернуть пустой файл
+                return cb(null, file);
+            }
+            //если пользователь не указал что удалить, то удаляем все
+            if (!("debugtype" in options)) {
+                options.debugtype = [
+                    "console",
+                    "window.console",
+                    "debbuger"
+                ];
+            }
+            console.log(options.debugtype);
+            //генерируем регулярное выражение
+            var regex_console = new RegExp(
+                generate_reg_string(options.debugtype),
+                "gi"
+            );
+            /*var regex_console = new RegExp(
+                (options.debugtype.indexOf( 'debugger' ) !== -1 ? "debugger;|" :
+                "(console|window.console)." +
+                    "(?:log|info|warn|error|assert|count|clear|group|groupEnd|groupCollapsed|trace|debug|dir|dirxml|profile|profileEnd|time|timeEnd|timeStamp|table|exception)" +
+                    "\\([^;]*\\);?"),
+                "gi"
+            );*/
+            console.log(regex_console);
+            const data = file.contents.toString()
+            // Заменяем информацию для отладки на пустую строку
+                .replace(regex_console,"");
+
+            // Загоняем строку обратно в буфер
+            file.contents = new Buffer(data);
+
+            // Передаём файл следующему плагину
+            this.push(file);
+        }
+
+        if (file.isStream()) {
+            var error= new PluginError(PLUGIN_NAME, 'Streams not supported!');
+            console.log(error.toString());
+            return cb();
+        }
+
+        // make sure the file goes through the next gulp plugin
+        this.push(file);
+        // tell the stream engine that we are done with this file
+        cb();
+    });
+
+    // returning the file stream
+    return stream;
+}
+
+// exporting the plugin main function
+module.exports = obrabot;
+
+
+
